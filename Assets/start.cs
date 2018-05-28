3a37c796b40be91543fe86c931562a2750cf5d64
Bassilij <fok-wassya@yandex.ru>
2018-05-28 22:02:57 +0300

    Initial commit

---
+++ Assets/start.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class start : MonoBehaviour {
+
+    GameObject C;
+    Vector3 xyz;
+
+    void Start () {
+        //C = GameObject.Find("cube1");
+        C = this.gameObject;
+        xyz.x = C.transform.position.x;
+        xyz.y = C.transform.position.y;
+        xyz.z = C.transform.position.z;
+        C.transform.position = xyz;
+    }
+
+    void Update() {
+        if(GameObject.Find("Camera").GetComponent<startgame>().a < 0)
+            Destroy(C);
+        float speed = 1;
+        if (xyz.x < -150)
+        {
+            Delete();
+            Destroy(C);
+        }
+        else
+            xyz.x = xyz.x - speed;
+        C.transform.position = xyz;
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(C);
+
+        if (C.GetComponent<Renderer>().material.color == Color.red)
+        {
+            GameObject Score;
+            Score = GameObject.Find("Score");
+            string s = Score.GetComponent<TextMesh>().text;
+            s = s.Substring(s.IndexOf(" "), s.Length - s.IndexOf(" "));
+            int a = int.Parse(s);
+            a++;
+            Score.GetComponent<TextMesh>().text = "Score: " + a;
+        }
+        if (C.GetComponent<Renderer>().material.color == Color.blue)
+        {
+            GameObject HP;
+            HP = GameObject.Find("HP");
+            string s = HP.GetComponent<TextMesh>().text;
+            s = s.Substring(s.IndexOf(" "), s.Length - s.IndexOf(" "));
+            int b = int.Parse(s);
+            b--;
+            HP.GetComponent<TextMesh>().text = "HP: " + b;
+
+            if (b == 0)
+            {
+                HP.GetComponent<TextMesh>().text = "YOU LOSE!!!";
+                LoseLevel();
+            }
+        }
+
+    }
+
+    void Delete() {
+        GameObject HP;
+        HP = GameObject.Find("HP");
+        string s = HP.GetComponent<TextMesh>().text;
+        if (s != "YOU LOSE!!!")
+        if (C.GetComponent<Renderer>().material.color == Color.red)
+        {
+                s = s.Substring(s.IndexOf(" "), s.Length - s.IndexOf(" "));
+                int b = int.Parse(s);
+                b--;
+                HP.GetComponent<TextMesh>().text = "HP: " + b;
+
+                if (b == 0)
+                {
+                    HP.GetComponent<TextMesh>().text = "YOU LOSE!!!";
+                    LoseLevel();
+                }
+            }
+    }
+
+    void LoseLevel()
+    {
+        //Destroy(this.gameObject);
+        GameObject.Find("Camera").GetComponent<startgame>().STOP();
+    }
+}
---
+++ Assets/startgame.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startgame : MonoBehaviour {
+    public int a = 0;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (a >= 0)
+        {
+            GameObject D;
+            //D = new GameObject();
+            D = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+            float r = Random.value;
+            if (r < 0.33)
+                D.GetComponent<Renderer>().material.color = Color.red;
+            if (r > 0.33 && r < 0.66)
+                D.GetComponent<Renderer>().material.color = Color.white;
+            if (r > 0.66)
+                D.GetComponent<Renderer>().material.color = Color.blue;
+            Vector3 sss;
+            sss.x = 10;
+            sss.y = 10;
+            sss.z = 10;
+            D.transform.localScale = sss;
+            Vector3 xyz;
+            xyz.x = 150;
+            xyz.y = -100 + Random.value*200;
+            xyz.z = Random.value * 20;
+            D.transform.position = xyz;
+            D.AddComponent<start>();
+
+            //a++;
+        }
+    }
+
+    public void STOP() {
+        a = -1;
+    }
+}
